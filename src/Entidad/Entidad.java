c8d8e07c855d1c46a4afd123d911aee59da0d863
unknown <diegohernanjuarez965@gmail.com>
2021-11-01 16:53:33 -0300

    Posicion y entidad

--- src/Entidad/Entidad.java
+++ src/Entidad/Entidad.java
@@ -1,16 +1,41 @@
 package Entidad;
-
 import Posicion.Posicion;
+import Visitor.Visitor;
 
 public abstract class Entidad {
 	
 	private Posicion p;
 	private String skin;
+	private Visitor visitor;
+	private int alto= 40;
+	private int ancho= 40;
+	
+	public Entidad(Posicion p,String skin,Visitor visitor) {
+		this.p=p;
+		this.skin=skin;
+		this.visitor=visitor;
+	}
 	
 	public Posicion obtenerPosicion() {
 		return p;
 	}
+	
 	public String obtenerSkin() {
 		return skin;
 	}
+	
+	public Visitor obtenerVisitor() {
+		return visitor;
+	}
+	
+	public int obtenerAlto() {
+		return alto;
+	}
+	
+	public int obtenerAncho() {
+		return ancho;
+	}
+	
+	public abstract void accept(Visitor v);
+	
 }
--- src/Posicion/PosicionLogica.java
+++ src/Posicion/PosicionLogica.java
@@ -1,5 +1,47 @@
 package Posicion;
 
 public class PosicionLogica {
-
+	
+	private int x,y;
+	private int ancho= 40;
+	private int alto= 40;
+	
+	public PosicionLogica(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+    public int obtenerX() {
+    	return x;
+    }
+	
+	public int obtenerY() {
+		return y;
+	}
+	
+	public int obtenerAncho() {
+		return ancho;
+	}
+	
+	public int obtenerAlto() {
+		return alto;
+	}
+	
+	public void setX(int x) {
+		this.x=x;
+	}
+	
+	public void setY(int y) {
+		this.y=y;
+	}
+	
+	public boolean colisionan(Posicion p) {
+		boolean toReturn=false;
+		if(p.obtenerX()==x && p.obtenerY()==y)
+			toReturn=true;
+		return toReturn;
+	}
+	
 }
+	
+	
