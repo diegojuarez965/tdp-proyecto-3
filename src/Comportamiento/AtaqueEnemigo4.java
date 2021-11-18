6be5ed5104f02db838932cd2381d85dbea6f369d
TomasHerg <89678459+TomasHerg@users.noreply.github.com>
2021-11-17 21:30:20 -0300

    Clyde

--- src/Comportamiento/AtaqueEnemigo4.java
+++ src/Comportamiento/AtaqueEnemigo4.java
@@ -13,6 +13,16 @@ public class AtaqueEnemigo4 extends Ataque {
 	@Override
 	public void moverSiguientePos() {
 		Posicion posPersonaje = enemigo.obtenerLaberinto().obtenerPersonajePrincipal().obtenerPosicion();
+		Posicion posEnemigo = enemigo.obtenerPosicion();
+		int personajeX = posPersonaje.obtenerX();
+		int personajeY = posPersonaje.obtenerY();
+		int enemigoX = posEnemigo.obtenerX();
+		int enemigoY = posEnemigo.obtenerY();
+		double distancia = Math.sqrt(Math.pow(enemigoX - personajeX, 2) + Math.pow(enemigoY - posPersonaje.obtenerY(), 2));
+		if(distancia/40 <= 4)
+			moverHacia(personajeX, personajeY);
+		else
+			moverHacia(0, 18);
 	}
 
 }
